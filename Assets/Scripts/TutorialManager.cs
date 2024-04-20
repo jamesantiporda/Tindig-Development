bd51cfc294632a2ed8c8cd56501d3b9fb3a99536
James Antiporda <james.antiporda@obf.ateneo.edu>
2024-04-20 22:59:52 +0800

    Added Success cooldown of 1 second to Training mode

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -33,66 +33,80 @@ public class TutorialManager : MonoBehaviour
         // Forward and Backwards
         if (tutorialStage == 0)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "movement";
             controls.text = "A - LEFT, D - RIGHT";
             moreTimes.text = 5 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 5)
             {
                 success = 0;
-                tutorialStage = 1;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Jumping
         if (tutorialStage == 1)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "jumping";
             controls.text = "W - jump";
             moreTimes.text = 3 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 3)
             {
                 success = 0;
-                tutorialStage = 2;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Crouching
         if (tutorialStage == 2)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "crouching";
             controls.text = "S - crouch";
             moreTimes.text = 3 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.S) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 3)
             {
                 success = 0;
-                tutorialStage = 3;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Sprinting
         if (tutorialStage == 3)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Sprinting";
             controls.text = "Forward X2 then Hold";
             moreTimes.text = 3 - success + " more time(s)";
@@ -107,13 +121,16 @@ public class TutorialManager : MonoBehaviour
             if (success >= 3)
             {
                 success = 0;
-                tutorialStage = 4;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Backdash
         if (tutorialStage == 4)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Back Dashing";
             controls.text = "Backward X2";
             moreTimes.text = 3 - success + " more time(s)";
@@ -128,207 +145,248 @@ public class TutorialManager : MonoBehaviour
             if (success >= 3)
             {
                 success = 0;
-                tutorialStage = 5;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Light
         if (tutorialStage == 5)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Attacks";
             controls.text = "J - Light Attack\nLow Damage, Fast Attack\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.J))
+            if (Input.GetKeyDown(KeyCode.J) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 6;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Medium
         if (tutorialStage == 6)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Attacks";
             controls.text = "K - Medium Attack\nMedium damage, Medium Speed\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.K))
+            if (Input.GetKeyDown(KeyCode.K) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 7;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Heavy
         if (tutorialStage == 7)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Attacks";
             controls.text = "L - Heavy Attack\nHigh damage, Slow Attack\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.L))
+            if (Input.GetKeyDown(KeyCode.L) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 8;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Overhead
         if (tutorialStage == 8)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Attacks";
             controls.text = "I - Overhead Attack\nHigh Damage, Goes through Low Guard\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.I))
+            if (Input.GetKeyDown(KeyCode.I) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 9;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Special
         if (tutorialStage == 9)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Attacks";
             controls.text = "M - Special Attack\nAttack Unique to Fighting Style\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKeyDown(KeyCode.M))
+            if (Input.GetKeyDown(KeyCode.M) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 10;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Lunge Attacks
         if (tutorialStage == 10)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Lunge Attacks";
             controls.text = "Forward + L (Heavy) - \nHigh Damage and Propels Player Forward\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (playerMovement.ReturnDirection() == 6 && Input.GetKeyDown(KeyCode.L))
+            if (playerMovement.ReturnDirection() == 6 && Input.GetKeyDown(KeyCode.L) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 11;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Crouch Attacks
         if (tutorialStage == 11)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Low Attacks";
             controls.text = "Crouch + J/K/L/I/M - Low Attack\n(Goes through normal Guard)\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 10 - success + " more time(s)";
 
-            if (Input.GetKey(KeyCode.S) && (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.M)))
+            if (Input.GetKey(KeyCode.S) && (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.M)) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 10)
             {
                 success = 0;
-                tutorialStage = 12;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Launcher
         if (tutorialStage == 12)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Launcher";
             controls.text = "Crouch + L (Heavy)\nLaunches opponent\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.L))
+            if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.L) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 13;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Sweep
         if (tutorialStage == 13)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Sweep";
             controls.text = "Crouch + Forward + L (Heavy)\nKnocks Down opponent\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 4 - success + " more time(s)";
 
-            if (Input.GetKey(KeyCode.S) && playerMovement.ReturnDirection() == 6 && Input.GetKeyDown(KeyCode.L))
+            if (Input.GetKey(KeyCode.S) && playerMovement.ReturnDirection() == 6 && Input.GetKeyDown(KeyCode.L) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 4)
             {
                 success = 0;
-                tutorialStage = 14;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Jump Attacks
         if (tutorialStage == 14)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Jump Attacks";
             controls.text = "Jump + J/K/L/I/M - Jump Attack\nTip: Attack Damage gets lower if same attack is used";
             moreTimes.text = 10 - success + " more time(s)";
 
-            if (!playerMovement.ReturnIsGrounded() && (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.M)))
+            if (!playerMovement.ReturnIsGrounded() && (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.M)) && !blockCooldown)
             {
                 success += 1;
                 successAnimator.SetTrigger("Success");
+                StartCoroutine(BlockCheckCooldown());
             }
 
             if (success >= 10)
             {
                 success = 0;
-                tutorialStage = 15;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
@@ -354,7 +412,8 @@ public class TutorialManager : MonoBehaviour
             if (success >= 3)
             {
                 success = 0;
-                tutorialStage = 16;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
@@ -380,11 +439,12 @@ public class TutorialManager : MonoBehaviour
 
             if (success >= 3)
             {
-                success = 0;
                 enemyMovement.SetWithinAttackRange(false);
                 enemyCombat.SetIsCPU(false);
                 enemyMovement.SetIsCPU(false);
-                tutorialStage = 17;
+                success = 0;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
@@ -404,16 +464,19 @@ public class TutorialManager : MonoBehaviour
             if (success >= 3)
             {
                 SpriteToPlayer.perfectGuard -= PerfectGuardSuccess;
-                success = 0;
                 enemyCombat.SetIsCPU(false);
                 enemyMovement.SetIsCPU(false);
-                tutorialStage = 18;
+                success = 0;
+                blockCooldown = false;
+                tutorialStage += 1;
             }
         }
 
         // Style Switching
         if (tutorialStage == 18)
         {
+            enemyCombat.SetIsCPU(false);
+            enemyMovement.SetIsCPU(false);
             title.text = "Style Switching";
             controls.text = "SHIFT + W/A/S/D - Switch Styles\nRequires unlocking Styles";
             moreTimes.text = 4 - success + " more time(s)";
@@ -426,8 +489,11 @@ public class TutorialManager : MonoBehaviour
 
             if (success >= 4)
             {
+                enemyCombat.SetIsCPU(false);
+                enemyMovement.SetIsCPU(false);
                 success = 0;
-                tutorialStage = 19;
+                blockCooldown = false;
+                tutorialStage += 1;
                 tutorialPanel.SetActive(false);
             }
         }
@@ -445,13 +511,30 @@ public class TutorialManager : MonoBehaviour
     public void BackOneStep()
     {
         success = 0;
-        tutorialStage -= 1;
+
+        if(tutorialStage <= 0)
+        {
+            tutorialStage = 0;
+        }
+        else
+        {
+            tutorialStage -= 1;
+        }
     }
 
     public void ForwardOneStep()
     {
         success = 0;
-        tutorialStage += 1;
+
+        if(tutorialStage == 18)
+        {
+            tutorialStage = 19;
+            tutorialPanel.SetActive(false);
+        }
+        else
+        {
+            tutorialStage += 1;
+        }
     }
 
     private IEnumerator BlockCheckCooldown()
